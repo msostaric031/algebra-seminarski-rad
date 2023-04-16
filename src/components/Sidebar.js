d5d38ed090f33ff386b01552d1a47cfd0ecae9c1
Marko <54621273+msostaric-hub@users.noreply.github.com>
2023-04-16 22:08:38 +0200

    edited Sidebar component, added css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,7 +12,6 @@ const Sidebar = () => {
       <div className="container-fluid mt-3">
         <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-md">
           <div className="container-fluid p-2">
-            <a className="navbar-brand text-primary mr-0">Company Logo</a>
             <div className="form-inline ml-auto">
               <div className="btn btn-primary" onClick={ToggleSidebar}>
                 <i className="fa fa-bars"></i>
@@ -20,7 +19,7 @@ const Sidebar = () => {
             </div>
           </div>
         </nav>
-        <div className={`sidebar ${isOpen == true ? "active" : ""}`}>
+        <div className={`sidebar ${isOpen === true ? "active" : ""}`}>
           <div className="sd-header">
             <h4 className="mb-0">Sidebar Header</h4>
             <div className="btn btn-primary" onClick={ToggleSidebar}>
@@ -30,34 +29,20 @@ const Sidebar = () => {
           <div className="sd-body">
             <ul>
               <li>
-                <a className="sd-link">Menu Item 1</a>
+                <a href="#" className="sd-link">
+                  Menu Item 1
+                </a>
               </li>
               <li>
-                <a className="sd-link">Menu Item 2</a>
-              </li>
-              <li>
-                <a className="sd-link">Menu Item 3</a>
-              </li>
-              <li>
-                <a className="sd-link">Menu Item 4</a>
-              </li>
-              <li>
-                <a className="sd-link">Menu Item 5</a>
-              </li>
-              <li>
-                <a className="sd-link">Menu Item 6</a>
-              </li>
-              <li>
-                <a className="sd-link">Menu Item 7</a>
-              </li>
-              <li>
-                <a className="sd-link">Menu Item 8</a>
+                <a href="#" className="sd-link">
+                  Menu Item 2
+                </a>
               </li>
             </ul>
           </div>
         </div>
         <div
-          className={`sidebar-overlay ${isOpen == true ? "active" : ""}`}
+          className={`sidebar-overlay ${isOpen === true ? "active" : ""}`}
           onClick={ToggleSidebar}
         ></div>
       </div>
